c195411bd2bdd988627f037a921230cd952e23a2
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-03 14:59:18 +0100

    seeder

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,7 +7,7 @@ const { getProducts, newProduct } = require('../controllers/productController')
 
 router.get('/products', getProducts);
 
-router.route('/product/new').post(newProduct)
+router.post('/product/new', newProduct)
 
 
 module.exports = router
