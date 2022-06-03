60b736998abb8eec1965a09c1c83121cae46a860
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-03 12:18:12 +0100

    product model

---
+++ backend/models/product.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxlength: [100, 'Product name cannot exceed 100 characters']
+    },
+    name: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxlength: [500, 'Product price cannot exceed 100 characters'],
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter product description'],
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select a category for this product'],
+        enum: {
+            values: [
+                'Electronics',
+                'Cameras',
+                'Laptop',
+                'Accessoires',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'Outdoor',
+                'Home'
+            ],
+            message: 'Please select correct category for product'
+        }
+    },
+    seller: {
+        type: String,
+        required: [true, 'Please enter product seller']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock'],
+        maxlength: [5, 'Product stock cannot exceed 5 characters'],
+        default: 0
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('Product', productSchema)
