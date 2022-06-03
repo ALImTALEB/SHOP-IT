17cb0eeaab6e616e3406f91a37c263e69ce30f24
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-03 17:01:00 +0100

    delete a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -71,3 +71,25 @@ exports.updateProduct = async (req, res, next) => {
 
     
 }
+
+// delete product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id)
+
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    await product.deleteOne()
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted.'
+    })
+
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
+const { getProducts,
+     newProduct,
+      getSingleProduct,
+       updateProduct,
+        deleteProduct
+     } = require('../controllers/productController')
 
 
 router.get('/products', getProducts);
@@ -11,8 +16,9 @@ router.post('/admin/product/new', newProduct)
 
 router.get('/product/:id', getSingleProduct)
 
-router.put('/admin/product/:id', updateProduct)
-
+router.route('/admin/product/:id')
+                 .put(updateProduct)
+                 .delete(deleteProduct)
 
 
 module.exports = router
