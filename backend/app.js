85f05622259a780088aa85fed575f1e971fd1da8
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-04 12:07:38 +0100

    errors middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 
+const errorMiddleware = require('./middlewares/errors')
+
 app.use(express.json());
 
 // import all routes
@@ -8,5 +10,8 @@ const products = require('./routes/product')
 
 
 app.use('/api/v1', products)
+
+// middleware to handle errors
+app.use(errorMiddleware)
 
 module.exports = app
