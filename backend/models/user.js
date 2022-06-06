16fa73fd6c346a0223a6447ed09434e0b1943843
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-06 12:40:47 +0100

    generate jwt token

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 
 const bcrypt = require('bcryptjs')
+const jwt= require('jsonwebtoken')
 
 
 const userSchema = new mongoose.Schema({
@@ -52,5 +53,12 @@ userSchema.pre('save', async function (next) {
 
     this.password = await bcrypt.hash(this.password, 10)
 })
+
+// return JWT token
+userSchema.methods.getJwtToken = () => {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_TIME
+    } )
+}
 
 module.exports = mongoose.model('User', userSchema)
