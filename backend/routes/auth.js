0150b3e611aeacfbbcbdad345db7ef5fddddf8ae
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-10 12:35:36 +0100

    users&user details

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword, updateProfile } = require('../controllers/authController')
+const { registerUser, loginUser, logout,
+     forgotPassword, resetPassword, getUserProfile,
+      updatePassword, updateProfile, allUsers, getUserDetails } = require('../controllers/authController')
 
-const { isAuthenticatedUser } = require('../middlewares/auth')
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
@@ -20,5 +22,8 @@ router.put('/password/update', isAuthenticatedUser, updatePassword)
 
 router.put('/me/update', isAuthenticatedUser, updateProfile)
 
+router.get('/admin/users', isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+
+router.get('/admin/user/:id', isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
 
 module.exports = router
