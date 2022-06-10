5464b201bb5d34650bc6563af37dcc92e0628d5d
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-10 12:09:10 +0100

    update userProfile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -171,3 +171,23 @@ exports.updatePassword = catchAsyncErrors(async(req, res,next)=> {
     sendToken(user, 200, res)
 
 })
+
+// update user profile => /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async(req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    // update avatar: to do later
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true
+    })
+})
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword } = require('../controllers/authController')
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword, updateProfile } = require('../controllers/authController')
 
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
@@ -17,6 +17,8 @@ router.put('/password/reset/:token', resetPassword)
 router.get('/me', isAuthenticatedUser, getUserProfile)
 
 router.put('/password/update', isAuthenticatedUser, updatePassword)
+
+router.put('/me/update', isAuthenticatedUser, updateProfile)
 
 
 module.exports = router
