77ccd10e5e5b4221040c336d872b0866e83affbc
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-16 14:05:39 +0100

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import Home from './components/Home'
+
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Home />
      <Footer />
     </div>
   );
