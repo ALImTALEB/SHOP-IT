f3813e793d62b075311bc364107fd5f6abb75b68
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-16 13:25:46 +0100

    Header&Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+
 function App() {
   return (
     <div className="App">
-     <h1>SHOP-IT</h1>
+     <Header />
+     <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+         <footer className="py-1">
+      <p className="text-center mt-1">
+        Shopping App - 2021-2022, All Rights Reserved
+      </p>
+    </footer>
+    </Fragment>
+  )
+}
+
+export default Footer
