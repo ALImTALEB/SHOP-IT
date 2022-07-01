3090d6dd28b88efe662baeec153d89d86a20bbac
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-30 22:36:48 -0500

    alerts

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,17 +7,26 @@ import Loader from "./layout/Loader";
 
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from "../actions/productActions";
-
+import { useAlert } from "react-alert";
 
 const Home = () => {
+
+  const alert = useAlert()
 
   const dispatch = useDispatch()
 
   const { loading, products, error, productsCount } = useSelector(state => state.products)
 
 useEffect( () => {
+
+  if (error) {
+   return alert.error(error)
+  }
+  
   dispatch(getProducts())
-}, [dispatch])
+
+
+}, [dispatch, alert ,error])
 
   return (
     <>
