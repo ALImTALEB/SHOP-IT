eb2fec44319bba2910d7195082d82b4fdd183224
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-07-01 09:13:28 -0500

    prodDetails component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+
 import Home from './components/Home'
+import ProductDetails from './components/product/ProductDetails';
 
 
 const App =() =>{
@@ -14,6 +16,7 @@ const App =() =>{
       <div className="container container-fluid">
      <Routes>
      <Route path="/" element={<Home />} />
+     <Route path="/product/:id" element={<ProductDetails />} />
      </Routes>
      </div>
      <Footer />
