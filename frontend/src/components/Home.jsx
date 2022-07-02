d480a9af32bccaf7c02ac2ec8cd6a7da734de1e5
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-07-02 10:00:27 -0500

    filter by price

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,6 +5,8 @@ import Pagination from "react-js-pagination";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
+
+
 import MetaData from "./layout/MetaData";
 import Product from "./product/Product";
 import Loader from "./layout/Loader";
@@ -18,7 +20,9 @@ import { useAlert } from "react-alert";
 const { createSliderWithTooltip } = Slider;
 const Range = createSliderWithTooltip(Slider.Range)
 
+
 const Home = () => {
+
   const [currentPage, setCurrentPage] = useState(1);
   const [price, setPrice] = useState([1, 1000]);
 
@@ -56,33 +60,37 @@ const Home = () => {
 
           <section id="products" className="container mt-5">
             <div className="row">
-
               {keyword ? (
                 <Fragment>
                   <div className="col-6 col-md-3 mt-5 mb-5 ">
-                    <Range
-                      marks={{
-                        1: `$1`,
-                        1000: `$1000`
-                      }}
-                      min={1}
-                      max={1000}
-                      defaultValue={[1, 1000]}
-                      tipFormatter={(value) => `$${value}`}
-                      tipProps={{
-                        placement: "top",
-                        visible: true,
-                      }}
-                      value={price}
-                      onChange={(price) => setPrice(price)}
-                    />
+                    <div className="px-5">
+
+                      <Range
+                        marks={{
+                          1: `$1`,
+                          1000: `$1000`
+                         
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        onChange={(price) => setPrice(price)}
+                        value={price}
+                        tipFormatter={(value) => `$${value}`}
+                      />
+                    </div>
                   </div>
 
                   <div className="col-6 col-md-9 ">
                     <div className="row">
-                      { products.map((product) => (
-                  <Product key={product._id} product={product} col={4} />
-                ))}
+                      {products.map((product) => (
+                        <Product key={product._id} product={product} col={4} />
+                      ))}
                     </div>
                   </div>
                 </Fragment>
