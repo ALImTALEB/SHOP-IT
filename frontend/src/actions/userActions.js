a09053d667938887015dd77c367948b0ab8ae27e
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-07-08 10:28:03 -0500

    logout user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -9,6 +9,8 @@ import {
     REGISTER_USER_FAIL,
     LOAD_USER_REQUEST,
     LOAD_USER_SUCCESS,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
     LOAD_USER_FAIL,
 
     CLEAR_ERRORS
@@ -90,7 +92,24 @@ export const loadUser = () => async (dispatch) => {
     }
 }
 
+// logout user
+export const logout = () => async (dispatch) => {
+    try {
 
+       await axios.get('/api/v1/logout')
+
+        dispatch({
+            type: LOGOUT_SUCCESS,
+        })
+        
+    } catch (error) {
+        dispatch({
+            type: LOGOUT_FAIL,
+            payload: error.response.data.message
+        })
+        
+    }
+}
 
 
 // clear errors
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,6 +8,8 @@ import {
     LOAD_USER_REQUEST,
     LOAD_USER_SUCCESS,
     LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
 
     CLEAR_ERRORS
 } from '../constants/userConstants'
@@ -34,6 +36,13 @@ export const authReducer = (state = {user: {}}, action ) => {
                 user: action.payload
             }  
             
+        case LOGOUT_SUCCESS:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                user: null
+            }
+                    
             case LOAD_USER_FAIL:
                 return {
                     loading: false,
@@ -51,6 +60,12 @@ export const authReducer = (state = {user: {}}, action ) => {
                 isAuthenticated: false,
                 user: null,
                 error: action.payload
+            }
+
+        case LOGOUT_FAIL:
+            return {
+                    ...state,
+                    error: action.payload
             }
 
         case CLEAR_ERRORS:
