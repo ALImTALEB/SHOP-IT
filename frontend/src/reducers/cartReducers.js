53f092863804f27b86b3156d9b1dd01bc0ed297f
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-09-28 21:19:32 +0100

    remove btn in cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../constants/cartConstants";
+import { ADD_TO_CART, REMOVE_CART_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = {cartItems: []}, action ) => {
     switch (action.type) {
@@ -19,6 +19,12 @@ export const cartReducer = (state = {cartItems: []}, action ) => {
                     ...state,
                     cartItems: [...state.cartItems, item]
                 }
+            }
+
+        case REMOVE_CART_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(i=> i.product !== action.payload)
             }
             
 
