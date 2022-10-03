b4c883e57d2e729c637488f6a51e029b557ba3d3
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-10-03 19:18:07 +0100

    review red&act

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,7 +6,11 @@ import { ALL_PRODUCTS_REQUEST,
        CLEAR_ERRORS,
        PRODUCT_DETAILS_REQUEST,
        PRODUCT_DETAILS_SUCCESS,
-       PRODUCT_DETAILS_FAIL
+       PRODUCT_DETAILS_FAIL,
+
+       NEW_REVIEW_REQUEST,
+       NEW_REVIEW_SUCCESS,
+       NEW_REVIEW_FAIL
      } from '../constants/productConstants'
 
 export const getProducts = (keyword = '' ,currentPage = 1, price, category, rating= 0) => async (dispatch) => {
@@ -57,6 +61,32 @@ export const getProductDetails = (id) => async (dispatch) => {
     }
 }
 
+
+export const newReview = (reviewData) => async (dispatch) => {
+    try {
+
+        dispatch({ type: NEW_REVIEW_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.put(`/api/v1/review`, reviewData, config)
+
+        dispatch({ 
+            type: NEW_REVIEW_SUCCESS,
+            payload: data.success
+         })
+
+    } catch (error) {
+        dispatch({
+            type: NEW_REVIEW_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
 
 // clear errors
 export const clearErrors = () => async (dispatch) => {
